c703956ae44c630dcc401a4e58679c36257753bf
Gary Kim <moduedu@gmail.com>
2020-07-16 12:05:59 +0900

    import 방법1

---
+++ app.py
@@ -0,0 +1,15 @@
+import func_module
+
+# func_module.module_show()
+
+nowdate = func_module.date_now()
+now_year = nowdate.year
+date_today = '{}년 {}월 {}일'.format(nowdate.year , nowdate.month ,nowdate.day)
+# print(nowdate)
+# print(now_year)
+print(date_today)
+
+x_mas = nowdate.replace(month=12 , day=25)
+
+date_xmas = '{}년 {}월 {}일'.format(x_mas.year , x_mas.month ,x_mas.day)
+print(date_xmas)
